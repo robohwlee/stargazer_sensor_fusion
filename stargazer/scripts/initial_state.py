5aa2809169bf4667e51802a165fce966ab5a5d07
HW-MLCS <lee11161116@gmail.com>
2020-03-04 22:03:45 +0900

    average init pose

--- stargazer/scripts/initial_state.py
+++ stargazer/scripts/initial_state.py
@@ -11,24 +11,31 @@ from geometry_msgs.msg import (Point, Quaternion, Pose, PoseArray,
                                Transform, TransformStamped,
                                PoseWithCovariance, PoseWithCovarianceStamped)
 from tf.transformations import euler_from_quaternion
+from auv_msgs.msg import RPY
 import numpy as np
 
 
-class PoseRepresent:
+class InitPoseCalculator:
 
     def __init__(self):
         
         # ROS parameters
-        # self.obstacle_topic = rospy.get_param("/obstacle_predictor/obstacle_topic")
-        self.step = []
-        self.count = 0
-        self.timer = 0
+        self.stargazer_frame_id = rospy.get_param('~stargazer_frame_id', 'stargazer')
+        self.map_frame_prefix = rospy.get_param('~map_frame_prefix', 'stargazer/map_')
+        self.marker_frame_prefix = rospy.get_param('~marker_frame_prefix', 'stargazer/marker_')
+        self._x = np.array([])
+        self._y = np.array([])
+        self._angle = np.array([])
+        self.pose_count = 0
+        self.count_limit = 50
 
         # Initialize ros node
         rospy.init_node('initial_pose_node', anonymous = True)
         # Subscriber
         rospy.Subscriber('marker_poses', PoseWithCovarianceStamped, self.markerCallback, queue_size=1)
-        
+        # Publisher
+        self.init_pose_pub = rospy.Publisher('init_pose', InitPose, queue_size=1)
+
 
     def spin(self):
         rospy.spin()
@@ -36,104 +43,52 @@ class PoseRepresent:
 
     def markerCallback(self, msg):
         
-        self.start_t = time.time()
         self.marker = msg
+        self.getPose()
 
 
-    def stackPose(self):
+    def getPose(self):
         '''
         estimate initial pose
         '''
-        _x = np.array([])
-        _y = np.array([])
-        _z = np.array([])
-        _euler = np.array([])      
-                
-        while:
-             
-        # _x.append(self.marker.pose.pose.position.x)
-        # _y.append(self.marker.pose.pose.position.y)
-        # _z.append(self.marker.pose.pose.position.z)
-
-        _x = np.append(_x, self.marker.pose.pose.position.x)
-        _y = np.append(_y, self.marker.pose.pose.position.y)
-        _z = np.append(_z, self.marker.pose.pose.position.z)
-
-        _pose = np.stack([_x,_y,_z], axis=0)
-
-        _quaternion = (self.marker.pose.pose.orientation.x,
-                        self.marker.pose.pose.orientation.y,
-                        self.marker.pose.pose.orientation.w,
-                        self.marker.pose.pose.orientation.z)
-
-        _roll, _pitch, _yaw = euler_from_quaternion(_quaternion)
-
-        _euler = np.stack([
-
-        self.averagedPose(_position, _euler)
-
+        self._x = np.append(self._x, self.marker.pose.pose.position.x)
+        self._y = np.append(self._y, self.marker.pose.pose.position.y)
         
+        _quaternion = (self.marker.pose.pose.orientation.x,
+                    self.marker.pose.pose.orientation.y,
+                    self.marker.pose.pose.orientation.w,
+                    self.marker.pose.pose.orientation.z)
+        _roll, _pitch, _yaw = euler_from_quaternion(_quaternion)
+        self._angle = np.append(_angle, _yaw)
 
+        self.pose_count += 1
+        
+        # _position = np.vstack([_x,_y])
+        if self.pose_count > self.count_limit:
+            _pose = np.stack([_x,_y,_angle], axis=0)
+            self.averagedPose(_pose)
 
+       
     def averagedPose(self, _pose):
         '''
         average stacked pose and publish
         '''
-        init_x = np.mean(_x)
-        init_y = np
-
-
-    def averageFunc(self, poselist):
-
-        AVG = sum(poselist, 0.0) / len(poselist)
-
-        return AVG
-
-
+        stamp = rospy.Time.now()
+        init_pose_msg = InitPose()
+        init_pose_msg.header.stamp = stamp
+        init_pose_msg.header.frame_id = stargazer
+        init_pose_msg.position.x = np.mean(_pose[0, 10:40])
+        init_pose_msg.position.y = np.mean(_pose[1, 10:40])
+        init_pose_msg.euler.yaw = np.mean(_pose[2, 10:40])
         
-    def poseGraph(self):
-        '''
-        draw the mesearued pose
-        '''
-        
-        # _x = []
-        # _y = []
-        
-        plt.ion()
-        
-        _x = self.marker.marker_poses[0].position.x
-        _y = self.marker.marker_poses[0].position.y
+        self.init_pose_pub.publish(init_pose_msg)
 
-        # for marker in self.marker.marker_poses:
-        
-        #     _x.append(marker.position.x)
-        #     _y.append(marker.position.x)
-            
-
-        plt.plot(_x, _y, '.b') # '.b' = blue point mark at start point
-        
-        plt.axis([0, 2, -1, 1]) # x axis from -2 to 2, y axis from 0 to 3
-        
-        plt.xlabel('horizontal(m)')
-        plt.ylabel('vertical(m)')
-        plt.title('map')
-        plt.legend(loc = 'upper right')
-        plt.draw()
-        plt.pause(0.01)
-        self.timer += 1
-        # if self.timer % 20 == 0:
-        #     plt.clf()
-        print("end of the graph")
-        # print("time: ", time.time() - self.start_t) # check calculated time
-
-
-##  End of class ObstacleRepresent.
 
 
 if __name__ == '__main__':
 
     try:
-        node = PoseRepresent()
+        node = InitPoseCalculator()
         node.spin()
     except rospy.ROSInterruptException:
 		rospy.loginfo("node terminated.")
